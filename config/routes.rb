4b5f1a19d275a60e92c1d6fa2eb80eeb9805822e
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-03-02 17:54:19 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :profiles, only: [:show]
+  resources :raclettemachines, only: [:index, :show, :new, :create] do
+    resources :reservations, only: [:new, :create]
+  end
+
+  resources :reservations, only: [:index]
+
+  namespace :owner do
+    resources :raclettemachines, only: [:index] # => /owner/raclette_machines
+    resources :reservations, only: [:index] do
+      member do
+        patch :accept # => /owner/reservations/:id/accept
+        patch :decline
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
