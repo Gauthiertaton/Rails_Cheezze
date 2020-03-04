678c210b5fd2929ab2385d069dbf4178881a4c68
Justine Barbauk <justinehsd@gmail.com>
2020-03-04 11:38:31 +0100

    add links to profile

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,6 +12,11 @@ class ReservationsController < ApplicationController
     redirect_to raclette_machines_path
   end
 
+  def show
+    @reservation = Reservation.find(params[:id])
+    @reservation = Reservation.new
+  end
+
   private
   def reservation_params
     params.require(:reservation).permit(:status, :total_price, :reservation_end_date, :reservation_start_date, :user_id, :raclette_machine_id)
