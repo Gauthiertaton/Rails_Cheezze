e4ff8d89564f604938dbde346977d0ead89d7e55
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-03-06 12:59:24 +0100

    modif nom status dans index

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -13,7 +13,7 @@ class ReservationsController < ApplicationController
     @reservation.user_id = current_user.id
     @reservation.raclette_machine = RacletteMachine.find(params[:raclette_machine_id])
     @reservation.total_price = (@reservation.reservation_end_date - @reservation.reservation_start_date).to_i * (@reservation.raclette_machine.price).to_i
-    @reservation.status = 'En attente'
+    @reservation.status = 'En attente de réservation'
     @reservation.save!
     redirect_to reservations_path
   end
