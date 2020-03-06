78fe34828c0bb698c14c73e4cc14f96188923199
Gauthier Taton <gauthier_taton@yahoo.fr>
2020-03-06 16:53:55 +0100

    init flatpickr in pack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,11 @@
 import "bootstrap";
-import "../plugins/flatpickr";
 import { calcTotalPrice } from "../components/total_price";
 import "../plugins/init_mapbox";
 // CSS
 import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
 import { initMapbox } from '../plugins/init_mapbox';
+import { initFlatpickr } from '../plugins/flatpickr';
 
 initMapbox();
+initFlatpickr();
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,11 +3,14 @@ import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 import { calcTotalPrice } from '../components/total_price';
 
+const initFlatpickr = () => {
+  flatpickr('#range_start', {
+    dateFormat: "d/m/Y",
+    plugins: [new rangePlugin({ input: '#range_end'})],
+    onChange: (selectedDates, dateStr, instance) => {
+      calcTotalPrice(selectedDates[0], selectedDates[1]);
+    }
+  });
+}
 
-flatpickr('#range_start', {
-  dateFormat: "d/m/Y",
-  plugins: [new rangePlugin({ input: '#range_end'})],
-  onChange: (selectedDates, dateStr, instance) => {
-    calcTotalPrice(selectedDates[0], selectedDates[1]);
-  }
-});
+export { initFlatpickr }
